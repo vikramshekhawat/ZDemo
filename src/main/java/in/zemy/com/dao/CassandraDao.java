5e355c08ac805c18ef1f596ab0c1ac77e0079f8d
Arindam Dutta <arindam.dutta@diginius.com>
2018-03-08 15:44:26 +0530

    second commit

--- src/main/java/in/zemy/com/dao/CassandraDao.java
+++ src/main/java/in/zemy/com/dao/CassandraDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import in.zemy.com.model.OrdersInfomodel;
 
 public interface CassandraDao {
-
+//hello
 	public void writeDataToMagentoOrdersCassandra(List<OrdersInfomodel> dataMagento);
 
 }
